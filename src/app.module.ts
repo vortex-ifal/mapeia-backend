98325b153b0a8f2d558b7f4e146d9063b9e9a72f
adelsonsljunior <junioradelsonst@gmail.com>
2026-05-18 13:02:54 -0300

    remove: default app module #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './api/modules';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
 })
 export class AppModule {}
